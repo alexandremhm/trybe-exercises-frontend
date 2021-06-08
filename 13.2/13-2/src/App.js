a375843cea90be56e42b59a50c8074a1bdf3f638
Marco Galindo <marco1408@gmail.com>
2021-06-08 14:59:06 -0300

    exercício 13.2

---
+++ 13.2/13-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Hello World</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
